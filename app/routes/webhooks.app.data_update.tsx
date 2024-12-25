1fbe84e16d58b5b6f685bdd3faad6aad2986e50e
tuanpa2295 <tuanpa2295@gmail.com>
2024-12-25 10:42:59 +0700

    Add route handle

--- app/routes/webhooks.app.data_update.tsx
+++ app/routes/webhooks.app.data_update.tsx
@@ -1,9 +1,41 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
-import db from "../db.server";
+import { createOrUpdate } from "app/models/order";
 
 export const action = async ({ request }: ActionFunctionArgs) => {
     const { payload, session, topic, shop } = await authenticate.webhook(request);
     console.log(`Received ${topic} webhook for ${shop}`);
+
+    switch (topic) {
+        case "ORDERS_CREATE":
+          console.log("ORDERS_CREATE event");
+          if (session) {
+            const {
+              id: orderId,
+              order_number: orderNumber,
+              total_price: totalPrice,
+              payment_gateway_names: paymentGateway,
+              tags,
+              customer: { email, first_name, last_name, default_address: { address1, city, country } }
+            } = payload as any;
+
+            const response = await createOrUpdate({
+              orderId: orderId.toString(),
+              orderNumber,
+              totalPrice,
+              paymentGateway: paymentGateway.join(","),
+              tags,
+              customerAddress: `${address1}, ${city}, ${country}`,
+              customerEmail: email,
+              customerFullName: `${first_name} ${last_name}`,
+            });
+            console.log(response);
+          }
+          break;
+        default:
+          console.log('Unsubscribed/Invalid topic!')
+      }
+
+
     return new Response();
 };
