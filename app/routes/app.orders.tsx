efc92efa918b04a04378b07df78ca9b528d9466b
tuanpa2295 <tuanpa2295@gmail.com>
2024-12-25 14:13:33 +0700

    Add fetching orders and showing tags as list

--- app/routes/app.orders.tsx
+++ app/routes/app.orders.tsx
@@ -1,48 +1,69 @@
 import { json } from "@remix-run/node";
 import { useLoaderData, Link } from "@remix-run/react";
 import { authenticate } from "../shopify.server";
-import { Card, EmptyState, Layout, Page, IndexTable } from "@shopify/polaris";
-import { Order } from "@prisma/client";
+import { Card, EmptyState, Layout, Page, IndexTable, List } from "@shopify/polaris";
+import type { Order } from "@prisma/client";
 import { getOrders } from "app/models/order";
 
 export async function loader({ request, params }: { request: any, params: any }) {
   const { admin, session } = await authenticate.admin(request);
   const orders = await getOrders();
 
+  const response = await admin.graphql(
+    `#graphql
+      query OrdersCount {
+        ordersCount(limit: 2000) {
+          count
+          precision
+      }
+    }`,
+  );
+
+  const { data: {
+    ordersCount: {
+      count
+    }
+  } }  = await response.json();
+
   return json({
     orders,
+    count,
   });
 }
 
 const EmptyQRCodeState = () => (
   <EmptyState image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png">
-    <p>Allow customers to scan codes and buy products using their phones.</p>
+    <p>No order found!</p>
   </EmptyState>
 );
 
 const OrderTable = ({ orders }: { orders: Order[] }) => (
-  <IndexTable
-    itemCount={orders.length}
-    headings={[
-      { title: "Order Id" },
-      { title: "Order Number" },
-      { title: "Customer Name" },
-      { title: "Total Price" },
-      { title: "Date created" },
-      { title: "Tags" },
-      { title: "" },
-    ]}
-    selectable={false}
-  >
-    {orders.map((order: Order) => (
-      <OrderTableRow order={order} key={order.id} />
-    ))}
-  </IndexTable>
+  <div style={{padding: '15px', height: '100%'}}>
+    <IndexTable
+      itemCount={orders.length}
+      headings={[
+        { title: "Order Id" },
+        { title: "Order Number" },
+        { title: "Customer Name" },
+        { title: "Email" },
+        { title: "Address" },
+        { title: "Total Price" },
+        { title: "Date created" },
+        { title: "Tags" },
+        { title: "" },
+      ]}
+      selectable={false}
+    >
+      {orders.map((order: Order) => (
+        <OrderTableRow order={order} key={order.id} />
+      ))}
+    </IndexTable>
+
+  </div>
 );
 
 const OrderTableRow = ({ order }: { order: Order }) => {
-  const orderUrl = `https://admin.shopify.com/store/tuanna2704-store1/orders/${order.orderId}`;
-
+  const orderUrl = `https://admin.shopify.com/store/tuanpa-application/orders/${order.orderId}`;
   return (
     <IndexTable.Row id={order.id.toString()} position={order.id}>
       <IndexTable.Cell>
@@ -52,77 +73,48 @@ const OrderTableRow = ({ order }: { order: Order }) => {
       </IndexTable.Cell>
       <IndexTable.Cell>{order.orderNumber}</IndexTable.Cell>
       <IndexTable.Cell>{order.customerFullName}</IndexTable.Cell>
+      <IndexTable.Cell>{order.customerEmail}</IndexTable.Cell>
+      <IndexTable.Cell>{order.customerAddress}</IndexTable.Cell>
       <IndexTable.Cell>{order.totalPrice}</IndexTable.Cell>
       <IndexTable.Cell>
         {new Date(order.createdAt).toDateString()}
       </IndexTable.Cell>
-      <IndexTable.Cell>{order.tags}</IndexTable.Cell>
       <IndexTable.Cell>
-        <Link to={`/app/orderEdit/${order.orderId}`}>Edit Tag</Link>
+        {order.tags}
+        {/* <List type="bullet">
+          {order.tags && order.tags?.split(',').length > 0 ? (
+            order.tags.split(',').map((tag, index) => (
+              <List.Item key={index}>{tag}</List.Item>
+            ))
+          ) : (
+              <></>
+            )}
+        </List> */}
+      </IndexTable.Cell>
+      <IndexTable.Cell>
+        <Link to={`/app/order/edit/${order.orderId}`}>Edit</Link>
       </IndexTable.Cell>
     </IndexTable.Row>
   );
 };
 
-const exportCSV = (orders: Order[]) => {
-  // Create a Blob object with the CSV data
-  const csvContent = orders
-    .map(
-      ({
-        orderId,
-        orderNumber,
-        totalPrice,
-        paymentGateway,
-        customerEmail,
-        customerFullName,
-        customerAddress,
-        tags,
-        createdAt,
-      }) =>
-        [
-          orderId,
-          orderNumber,
-          totalPrice,
-          paymentGateway,
-          customerEmail,
-          `"${customerFullName}"`,
-          `"${customerAddress}"`,
-          `"${tags}"`,
-          createdAt,
-        ].join(","),
-    )
-    .join("\n");
-  const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
-
-  // Create a temporary anchor element and trigger a download
-  const link = document.createElement("a");
-  if (link.download !== undefined) {
-    // feature detection
-    const url = URL.createObjectURL(blob);
-    link.setAttribute("href", url);
-    link.setAttribute("download", "orders.csv");
-    link.style.visibility = "hidden";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
-};
 export default function Index() {
   const { orders }: { orders: any[] } = useLoaderData();
+  const { count } : { count: number } = useLoaderData();
   return (
     <Page>
-      <ui-title-bar title="Order Table">
-        <button variant="primary" onClick={() => exportCSV(orders)}>
-          Export To CSV
-        </button>
-      </ui-title-bar>
       <Layout>
         <Layout.Section>
           <Card padding="0">
             {orders.length === 0 ? (
               <EmptyQRCodeState />
             ) : (
-              <OrderTable orders={orders} />
+              <>
+                <div style={
+                  {padding: '15px'}
+                }> Showing {orders.length} out of total { count } { count > 1 ? 'orders' : 'order' } created</div>
+                <OrderTable orders={orders} />
+              </>
             )}
           </Card>
         </Layout.Section>
