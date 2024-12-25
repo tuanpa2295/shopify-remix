c4d76cfd2851b923d5edd83db8fb754bbb59900a
tuanpa2295 <tuanpa2295@gmail.com>
2024-12-25 10:38:02 +0700

    Add orders webhook

---
+++ app/routes/webhooks.app.data_update.tsx
@@ -0,0 +1,9 @@
+import type { ActionFunctionArgs } from "@remix-run/node";
+import { authenticate } from "../shopify.server";
+import db from "../db.server";
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+    const { payload, session, topic, shop } = await authenticate.webhook(request);
+    console.log(`Received ${topic} webhook for ${shop}`);
+    return new Response();
+};
